4dbeea85f9b9967f4ff6c80cb84dff134a7aa2cd
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-24 16:26:27 +0200

    feat: add mail control

--- Mail/JS/mail.js
+++ Mail/JS/mail.js
@@ -5,5 +5,12 @@ const emailList = ["a@email.com","b@email.com","c@email.com","d@email.com","e@em
 const userEmail = prompt("inserisci la tua email")
 
 
-
+//controllo email
+if ((userEmail =="a@email.com") || (userEmail =="b@email.com") || (userEmail =="c@email.com") || (userEmail =="d@email.com") || (userEmail =="e@email.com")) {
+    alert("la tua email ha ottenuto l'accesso")
+    console.log(userEmail);
+}
+else {
+    alert("la tua email NON ha ottenuto l'accesso")
+}
 
