01acf93ff40109de7e7960a9f21905d20278df15
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-24 17:23:19 +0200

    feat: add dice variables

---
+++ Dice/JS/dice.js
@@ -0,0 +1,6 @@
+//variabili numeri
+const userNumber = parseInt (Math.random() * 6) + 1
+const computerNumber = parseInt (Math.random() * 6) + 1
+
+console.log(userNumber);
+console.log(computerNumber);
